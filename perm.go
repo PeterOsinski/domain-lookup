d3c16369b117384093d40c08005b7264d6fdf3f4
spinache <posinsk+github@gmail.com>
2015-05-27 16:54:01 +0200

    Update perm.go

--- perm.go
+++ perm.go
@@ -2,11 +2,10 @@ package main
 
 import "math"
 import "strings"
-import "container/heap"
-import "sync"
 
 var positions []int
 var chars []string
+var current float64
 
 func inc(pos int) bool {
 
@@ -24,27 +23,31 @@ func inc(pos int) bool {
 	return true
 }
 
-func getPermutations(capacity int, entropia string, mutex *sync.Mutex) {
+var a float64
+var b float64
 
+func initPermutations(capacity int, entropia string) {
 	positions = make([]int, capacity)
 	chars = strings.Split(entropia, "")
 
-	a := float64(capacity)
-	b := float64(len(entropia))
-	var i float64
+	a = float64(capacity)
+	b = float64(len(entropia))
+}
 
-	for i = 0; i < math.Pow(b, a); {
+func getPermutation() string{
 
-		var str = make([]string, capacity)
-		for k, v := range positions {
-			str[k] = chars[v]
-		}
-
-		mutex.Lock()
-		heap.Push(h, strings.Join(str, ""))
-		mutex.Unlock()
-
-		inc(len(positions) - 1)
-		i++
+	if current >= math.Pow(b, a) {
+		return ""
 	}
+
+	var str = make([]string, len(positions))
+	for k, v := range positions {
+		if v > 0 {
+			str[k] = chars[v-1]
+		}
+	}
+	inc(len(positions) - 1)
+	current++
+	
+	return strings.Join(str, "")
 }
